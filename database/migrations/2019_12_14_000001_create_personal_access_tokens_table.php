80fc23859c3028d2fe76fccc85177aac6c1596fe
Sanand S <inksplashx@example.com>
2025-07-17 22:34:02 +0530

    Add dummy migration to bypass Sanctum issue

---
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        // Dummy migration - do nothing
+    }
+
+    public function down(): void
+    {
+        // Dummy rollback - do nothing
+    }
+};
