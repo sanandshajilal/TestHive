1adfb362e09642c1eea15d2b463d8084e3538954
Sanand S <inksplashx@example.com>
2026-08-25 13:28:42 +0530

    T

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -438,7 +438,11 @@ private function storeScenario(Request $request)
         $scenario->marks = 0;
 
         $scenario->save();
-        dd('PARENT SAVED', $scenario->id);
+        
+        foreach ($request->input('child_questions', []) as $index => $childData) {
+
+    dd('CHILD LOOP REACHED', $index, $childData);
+}
 
         foreach ($request->input('child_questions', []) as $childData) {
 
